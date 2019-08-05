68136be17b7f7dad77bcbc3ca956bdba6b2d493a
unknown <919624032@qq.com>
2019-08-05 23:01:15 +0800

    detail page 完成!

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -6,5 +6,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    re_path(r'^goods/(?P<pk>\d+)$', views.DetailView.as_view(), name='detail')
+    re_path(r'^goods/(?P<pk>\d+)$', views.DetailView.as_view(), name='detail'),
+    re_path(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$', views.ListView.as_view(), name='list')
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.paginator import Paginator
 from django.views import View
 from apps.goods.models import *
 from apps.order.models import OrderGoods
@@ -51,7 +52,74 @@ class DetailView(View):
         # 获取商品的评论
         sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
         # 获取新品信息
-        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[0:2]
+
+        # 获取同一商品spu的其他规格商品
+        same_spu_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(pk=pk)
+
+        # 获取购物车商品数量
+        cart_count = 0
+        if request.user.is_authenticated:
+            coon = get_redis_connection('default')
+            cart_key = 'cart_%d' % request.user.id
+            cart_count = coon.hlen(cart_key)
+
+            # 添加用户的历史浏览记录
+            coon = get_redis_connection('default')
+            history_key = 'history_%d' % request.user.id
+            # 移除之前的该商品id
+            coon.lrem(history_key, 0, pk)
+            # 把goods_id左侧插入redis列表
+            coon.lpush(history_key, pk)
+            # 取用户保存的最新5条信息
+            coon.ltrim(history_key, 0, 4)
+
+        context = {
+            'sku': sku,
+            'types': types,
+            'sku_order': sku_orders,
+            'new_skus': new_skus,
+            'cart_count': cart_count,
+            'same_spu_skus': same_spu_skus
+        }
+
+        return render(request, 'detail.html', context)
+
+
+class ListView(View):
+    def get(self, request, type_id, page):
+        # 获取种类信息
+        try:
+            type = GoodsType.objects.get(pk=type_id)
+        except GoodsType.DoesNotExist:
+            return redirect(reverse('goods:index'))
+
+        types = GoodsType.objects.all()
+
+        sort = request.GET.get('sort', 'default')
+        if sort == 'price':
+            skus = GoodsSKU.objects.filter(type=type).order_by('price')
+        elif sort == 'hot':
+            skus = GoodsSKU.objects.filter(type=type).order_by('sales')
+        else:
+            skus = GoodsSKU.objects.filter(type=type).order_by('-id')
+
+        paginator = Paginator(skus, 1)
+
+        # 对页码进行容错处理
+        try:
+            page = int(page)
+        except Exception:
+            page = 1
+
+        if page > paginator.num_pages:
+            page = paginator.num_pages
+
+        skus_page = paginator.page(page)
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=type).order_by('-create_time')[0:2]
+
         # 获取购物车商品数量
         cart_count = 0
         if request.user.is_authenticated:
@@ -60,11 +128,12 @@ class DetailView(View):
             cart_count = coon.hlen(cart_key)
 
         context = {
-            'sku': sku,
+            'type': type,
             'types': types,
-            'sku_order': sku_orders,
+            'skus_page': skus_page,
             'new_skus': new_skus,
             'cart_count': cart_count,
+            'sort': sort
         }
 
-        return render(request, 'detail.html', context)
+        return render(request, 'list.html', context)
